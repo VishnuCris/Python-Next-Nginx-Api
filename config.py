4b808ad153dc2fe914e15868c521842ab8ade747
vishnu cris <vishnucris@gmail.com>
2023-02-09 22:08:15 +0530

    flask completed

--- config.py
+++ config.py
@@ -2,4 +2,5 @@
 class Config(object):
 	import os
 	BASE_PATH = os.path.abspath(os.path.dirname(__file__))
-	ENV = 'devolopment'
+	ENV = 'development'
+	FLASK_APP = 'Python_Next_Nginx_Api'
--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 # from app import app
 from Python_Next_Nginx_Api import app
+from waitress import serve
 
 if __name__ == '__main__':
-	app.run(debug=True,port=5001)
+	if app.config['ENV'] == 'production':
+		serve(app,host='127.0.0.1',port=5001)
+	else:
+		app.run(debug=True,port=5001)
